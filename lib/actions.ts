2c69858aa23514c70a85a549be1e0ad8322d99a7
shripal <shripalp@gmail.com>
2023-06-29 16:13:02 -0600

    auth2

---
+++ lib/actions.ts
@@ -0,0 +1,25 @@
+import { GraphQLClient } from "graphql-request";
+
+const isProduction = process.env.NODE_ENV === "production";
+const apiUrl = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
+  : "http://127.0.0.1:4000/graphql";
+
+const apiKey = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
+  : "letmein";
+
+const serverUrl = isProduction
+  ? process.env.NEXT_PUBLIC_SERVER_URL
+  : "http://localhost:3000";
+
+const client = new GraphQLClient(apiUrl);
+
+const makeGraphQLRequest = async (query: string, variables = {}) => {
+  try {
+    //client request
+    return await client.request(query, variables);
+  } catch (error: any) {
+    throw error;
+  }
+};
